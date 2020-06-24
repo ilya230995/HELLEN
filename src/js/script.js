82a028f38fd28e43ed89d92aaceb3461cead8de1
Ilya <ilyalysenko233@gmail.com>
2020-06-24 19:46:39 +0300

    add nav_link

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.menu-list'),
     hamburger = document.querySelector('.hamburger'),
     modal = document.querySelector('.modal-btn_white'),
+    menuItem = document.querySelectorAll('.menu-list__item'),
     logo = document.querySelector('.site-logo_header');
 
     hamburger.addEventListener('click', () => {
@@ -9,10 +10,10 @@ window.addEventListener('DOMContentLoaded', () => {
         hamburger.classList.toggle('hamburger_active');
         modal.classList.toggle('modal-btn_white_active');
         logo.classList.toggle('site-logo_header_active');
-    });
+});
 
     menuItem.forEach(item => {
-        item.addEventListener('click', () => {            
+        item.addEventListener('click', () => {         
             menu.classList.toggle('menu-list_active');
             hamburger.classList.toggle('hamburger_active');
             modal.classList.toggle('modal-btn_white_active');
